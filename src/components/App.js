052a4624a9fbaa5a7b216474afe53dfcda6963d5
tonystark <chandravicky288@gmail.com>
2023-06-09 17:33:16 +0530

    f

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,19 @@
-
-import React from "react";
-import './../styles/App.css';
+import React from 'react';
+import Tabs from './Tabs';
 
 const App = () => {
+  const tabs = [
+    { title: 'Tab 1', content: 'Content for Tab 1' },
+    { title: 'Tab 2', content: 'Content for Tab 2' },
+    { title: 'Tab 3', content: 'Content for Tab 3' },
+  ];
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <h1>My Tabs Component</h1>
+      <Tabs tabs={tabs} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
